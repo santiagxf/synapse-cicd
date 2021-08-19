4b8777077667b51afcbcc7e55875a42f2734093b
santiagxf <santiagof@outlook.com>
2021-08-19 11:37:15 -0300

    fix: change schema

--- src/AdventureWorks/dbo/Tables/DimTempTable.sql
+++ src/AdventureWorks/dbo/Tables/DimTempTable.sql
@@ -1,7 +1,6 @@
 CREATE TABLE [dbo].[DimTempTable] (
     [id]       INT          NOT NULL,
-    [lastName] VARCHAR (20) NULL,
-    [zipCode]  VARCHAR (6)  NULL
+    [lastName] VARCHAR (20) NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([id]));
 
