8feb8bde97e54d68caf98bfb2b9b7c738c8023a4
santiagxf <santiagof@outlook.com>
2021-08-20 13:40:09 -0300

    schema: changes

--- src/AdventureWorks/dbo/Tables/DimTempTable.sql
+++ src/AdventureWorks/dbo/Tables/DimTempTable.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [dbo].[DimTempTable] (
     [id]       INT          NOT NULL,
     [lastName] VARCHAR (20) NULL,
-    [zipCode]  VARCHAR (6)  NULL,
-    [city]     VARCHAR (20) NOT NULL
+    [zipCode]  VARCHAR (6)  NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([id]));
 
